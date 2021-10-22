a9d926a9ac1cd0f7cabd5bad62443e9d29969e1f
David Onder <donder@haywood.edu>
2021-10-22 09:02:20 -0400

    Remove extra call to setup.py()

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup()
+    #    setuptools.setup()
     
     with open("README.md", "r", encoding="utf-8") as fh:
         long_description = fh.read()
