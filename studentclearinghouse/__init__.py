9df3e5d9eb8f1aa1777fc0447887aeefd8332a72
David Onder <donder@haywood.edu>
2023-11-29 14:00:57 -0500

    Add nsc_return_se_convert to __init__.py.

--- studentclearinghouse/__init__.py
+++ studentclearinghouse/__init__.py
@@ -4,3 +4,4 @@ from studentclearinghouse.__version__ import __version__
 from .nsc_error import NSCError
 from .nsc_error import NSCConfigurationError
 from .nsc_request import NSCRequest
+from .nsc_return_se_convert import nsc_return_se_convert
